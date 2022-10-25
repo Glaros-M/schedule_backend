a1b4a97937814fbef009bf4af0a3b54945c3f29b
glaros-m <glaros-m@yandex.ru>
2022-10-25 20:57:36 +0300

    Не решил пока задачу построения вьюхи. Сложно. туплю((

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import models
 from jinja2 import Template
+from typing import NamedTuple
 
 
 def get_data_from_file(path: str) -> models.JSON_Faculties:
@@ -18,7 +19,7 @@ def search_by_group_name(group_name: str, all_files: list[models.JSON_Faculties]
                     print(group)
                     return group
                 else:
-                    #raise Exception
+                    # raise Exception
                     print("404 Not Found")
                     return None
 
@@ -30,6 +31,19 @@ def get_group_names_list(all_files: list[models.JSON_Faculties]) -> list[str]:
             for group in facultie.groups:
                 result.append(group.group_name)
     return result
+
+
+class DataForView(NamedTuple):
+    # TODO: Перенести класс и функцию куданадо (придумать куда)
+    rows: int
+    time: int
+    time_start: str
+    time_end: str
+    subject: str
+    type: str
+    subgroup: int
+    teachers: str
+    auditories: str
 
 
 def get_data_list_for_view(group: models.Groups) -> Template:
@@ -52,29 +66,47 @@ def get_data_list_for_view(group: models.Groups) -> Template:
 
     """
 
-    group.group_name
+    group_name = group.group_name
     for day in group.days:
-        day.weekday
+        weekday = day.weekday
         date = day.lessons[0].date
-        for lesson in day.lessons:
-            lesson.time
-            lesson.time_start
-            lesson.time_end
-            lesson.subject
-            lesson.type
-            lesson.subgroup
-            for teacher in lesson.teachers:
-                teacher.teacher_name
-            for auditorie in lesson.auditories:
-                auditorie.auditory_name
 
+        # TODO: Нада сделать както сортировку массива lessons. По параметру time наверное
+
+        def _get_list_from_lesson(lesson: models.Lessons) -> DataForView:
+            teachers_str = ""
+            for teacher in lesson.teachers:
+                teachers_str += teacher.teacher_name
+            auditories_str = ""
+            for auditorie in lesson.auditories:
+                auditories_str += auditorie.auditory_name
+
+            data = DataForView(
+                rows=0,
+                time=lesson.time,
+                time_start=lesson.time_start,
+                time_end=lesson.time_end,
+                subject=lesson.subject,
+                type=lesson.type,
+                subgroup=lesson.subgroup,
+                teachers=teachers_str,
+                auditories=auditories_str)
+
+            return data
+
+        for i in range(len(day.lessons) - 1):
+            lesson = day.lessons[i]
+            view_data = _get_list_from_lesson(lesson)
+            #TODO: Ну и как тут блин решать вопрос с обьединением строк? Либо считать по каунт, добавлять словарь, либо еще как то. я хз
+
+            
 
     return [""]
 
 
-if __name__=="__main__":
-    #data = get_data_from_file("rasp.json")
-    #search_by_group_name('АС2-221-ОБ', [data])
+if __name__ == "__main__":
+    # data = get_data_from_file("rasp.json")
+    # search_by_group_name('АС2-221-ОБ', [data])
     temp = Template("""
     {% if name %}
     <p>{{name}}</p>
@@ -83,6 +115,6 @@ if __name__=="__main__":
     {% endif %}
     """)
     print(temp.render(name=True))
-    #for name in get_group_names_list([data]):
+    # for name in get_group_names_list([data]):
     #    print(name)
-    #print(get_group_names_list([data]))
+    # print(get_group_names_list([data]))
